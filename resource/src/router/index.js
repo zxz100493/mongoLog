84120b5928dce3b13afb7e4c4189ae26ba162395
zxz100493 <zxz100493@163.com>
2022-07-14 23:36:50 +0800

    feat:change vue config

--- resource/src/router/index.js
+++ resource/src/router/index.js
@@ -8,6 +8,11 @@ const routes = [
     component: HomeView
   },
   {
+    path: '/',
+    name: 'box',
+    component: () => import(/* webpackChunkName: "about" */ '../views/BoxView.vue')
+  },
+  {
     path: '/about',
     name: 'about',
     // route level code-splitting
